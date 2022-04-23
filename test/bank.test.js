3ce7694456ff80dead596f8587d15e5060dbd120
Kathleen Ly <heykathl@users.noreply.github.com>
2022-04-23 15:38:08 +0100

    Refactored test to include a before each block

--- test/bank.test.js
+++ test/bank.test.js
@@ -1,23 +1,25 @@
 const Bank = require('../lib/bank')
 
 describe("Bank", () => {
+  let bank;
+
+  beforeEach(() => {
+    bank = new Bank();
+  });
 
   it("it returns bank balance", () => {
-    const bank = new Bank();
     expect(bank.getBalance()).toBe(0)
   });
 
   it("can deposit money into the bank account", () => {
-    const bank = new Bank();
     bank.deposit(10);
     expect(bank.getBalance()).toBe(10);
   });
 
   it("can withdrawn money from the bank account", () => {
-    const bank = new Bank();
     bank.deposit(10);
     bank.withdraw(5);
     expect(bank.getBalance()).toBe(5);
   });
-  
+
 })
