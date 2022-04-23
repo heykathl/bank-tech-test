8451e3ac4dd35109f5a1d8ec4317ddcb621421a5
Kathleen Ly <heykathl@users.noreply.github.com>
2022-04-23 15:32:11 +0100

    Implemented deposit method and testing

--- test/bank.test.js
+++ test/bank.test.js
@@ -5,5 +5,11 @@ describe("Bank", () => {
   it("it returns bank balance", () => {
     const bank = new Bank();
     expect(bank.getBalance()).toBe(0)
+  });
+
+  it("can deposit money into the bank account", () => {
+    const bank = new Bank();
+    bank.deposit(10);
+    expect(bank.getBalance()).toBe(10)
   })
 })
