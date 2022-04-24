72540c2b7bea17d894991ec98c8f4494b12c9930
Kathleen Ly <heykathl@users.noreply.github.com>
2022-04-24 16:13:18 +0100

    Changed the way transactions are stored, using a hash as opposed to array & wrote associated tests

--- test/bank.test.js
+++ test/bank.test.js
@@ -11,19 +11,47 @@ describe("Bank", () => {
     expect(bank.getBalance()).toBe(0)
   });
 
-  it("can deposit money into the bank account", () => {
-    bank.deposit(10);
-    expect(bank.getBalance()).toBe(10);
+  it("can deposit money into the bank account and balance is updated", () => {
+    bank.deposit(10.00);
+    expect(bank.getBalance()).toBe(10.00);
   });
 
-  it("can withdrawn money from the bank account", () => {
-    bank.deposit(10);
-    bank.withdraw(5);
-    expect(bank.getBalance()).toBe(5);
+  it("can withdraw money from the bank account and balance is updated", () => {
+    bank.deposit(10.00);
+    bank.withdraw(5.00);
+    expect(bank.getBalance()).toBe(5.00);
   });
 
-  it("stores the date of when the money was deposited into bank", () => {
-   bank.deposit(10, "14/01/2023");
-   expect(bank.getTransaction()).toEqual([["14/01/2023", 10]]);
+  it("keeps a list of the transactions made", () => {
+    bank.deposit(10.00);
+    bank.deposit(5.00);
+    expect(bank.getTransactions()).toEqual([
+      {amount: 10.00},
+      {amount: 5.00}
+    ])
   })
+
+  // it("stores the date of when the money was deposited into bank", () => {
+  //  bank.deposit(10, "14/01/2023");
+  //  expect(bank.getTransactions()).toEqual([["14/01/2023", 10]]);
+  // });
+
+  // it("stores the date of when the money is withdrawn from bank", () => {
+  //   bank.deposit(10, "14/01/2023");
+  //   bank.withdraw(5, "15/01/2023");
+  //   expect(bank.getTransactions()).toEqual([["14/01/2023", 10], ["15/01/2023", 5]])
+  // });
+
+  // it("stores all transactions on same day together", () => {
+  //   bank.deposit(10, "14/01/2023");
+  //   bank.withdraw(5, "14/01/2023");
+  //   expect(bank.getTransactions()).toEqual([["14/01/2023", 10, 5]])
+  // });
+
+  // it("transactions to display whether the transaction was a credit or debit", () => {
+  //   bank.deposit(10, "14/01/2023");
+  //   expect(bank.getTransactions()).toEqual([{
+  //     type: "credit",
+  //     amount: 10}])
+  // })
 })
