d03898fdb7f67a02ef0331fb8ed6edf78b6c8980
Kathleen Ly <heykathl@users.noreply.github.com>
2022-04-25 18:39:14 +0100

    README added and eslint installed

--- README.md
+++ README.md
@@ -1,1 +1,90 @@
 # Bank Tech Test
+
+A program which allows users to deposit and withdraw money; and print out a statement displaying all the transactions made.
+
+## How to install
+Fork or clone the respository to your local machine.
+`git clone https://github.com/heykathl/bank-tech-test`
+Once the repository has been cloned you will be able to install the dependencies.
+`npm install`
+
+## How to use
+Run `node`
+Set up the files and classes
+```
+> const Bank = require("./lib/bank");
+undefined
+> const Statement = require("./lib/statement");
+1650902753769
+undefined
+> const statement = new Statement();
+undefined
+> const bank = new Bank(statement);
+``` 
+
+To deposit money into the account
+`bank.deposit(<insert amount as an integer>, <insert date as a string>)`
+An example is shown below, and as shown in the Bank object, the transaction has been stored and balance updated
+```
+> bank.deposit(10, "14/02/2022")
+> bank.withdraw(5.5, "14/02/2022")
+undefined
+> bank
+Bank {
+  balance: 10,
+  transactions: [
+    {
+      type: 'credit',
+      date: '14/02/2022',
+      amount: '10.00',
+      balance: 10
+    },
+    { type: 'debit', date: '14/02/2022', amount: '5.50', balance: 4.5 }
+  ],
+  statement: Statement {
+    statement: [],
+    header: 'date || credit || debit || balance'
+  }
+}
+```
+To print a statement, run the following:
+```
+> bank.printStatement();
+'date || credit || debit || balance\n' +
+  '14/02/2022 || 10.00 ||  || 10.00\n' +
+  '14/02/2022 ||  || 5.50 || 4.50'
+```
+
+## Testing
+There are a series of tests which can be run with the following.
+Change into the relevant directory with `cd bank-tech-test`
+Run `jest` 
+To run tests from specific class files, this can be done by:
+`jest test/<filename>.test.js`
+
+## My approach to the challenge
+The challenge began with me drafting the user stories for each requirement outlined [here](https://github.com/makersacademy/course/blob/main/individual_challenges/bank_tech_test.md). 
+I began diagramming the Bank class, and its associated methods. During the process, I realised that it may have been better to make a new class for print the statement of transactions, as shown below. This is to ensure that me code follows single resposibility principle (SRP).
+<img width="467" alt="Screenshot 2022-04-25 at 18 31 05" src="https://user-images.githubusercontent.com/74867241/165142380-2c53bab0-56b4-4146-8773-3e7da5420617.png">
+
+## User Stories
+```
+As a customer,
+So that I can save my money to save up for nice things,
+I would like to deposit money into my bank account.
+```
+```
+As a customer,
+To treat myself to a new pair of shoes,
+I would like to withdraw money from my bank account.
+```
+```
+As a customer,
+So that I know whether I can afford that pair of shoes,
+I would like to be able to see my balance.
+```
+```
+As a customer,
+So I can keep track of my saving and spending habits,
+I would like to see a statement of all of my deposit and withdrawals
+```
--- test/bank.test.js
+++ test/bank.test.js
@@ -1,28 +1,29 @@
-const Bank = require('../lib/bank');
-const Statement = require('../lib/statement');
+/* eslint-disable no-undef */
+const Bank = require("../lib/bank");
+const Statement = require("../lib/statement");
 
 describe("Bank", () => {
   let bank;
   let statement;
 
   beforeEach(() => {
-    statement = new Statement()
+    statement = new Statement();
     bank = new Bank(statement);
   });
 
-  it("it returns bank balance", () => {
-    expect(bank.getBalance()).toBe(0)
-  });
+  // it("it returns bank balance", () => {
+  //   expect(bank.getBalance()).toBe(0)
+  // });
 
   it("can deposit money into the bank account and balance is updated", () => {
-    bank.deposit(10.50);
-    expect(bank.getBalance()).toBe(10.50);
+    bank.deposit(10.5);
+    expect(bank.getBalance()).toBe(10.5);
   });
 
   it("can withdraw money from the bank account and balance is updated", () => {
-    bank.deposit(10.50);
+    bank.deposit(10.5);
     bank.withdraw(5);
-    expect(bank.getBalance()).toBe(5.50);
+    expect(bank.getBalance()).toBe(5.5);
   });
 
   // it("keeps a list of each depositing transaction made", () => {
@@ -46,37 +47,37 @@ describe("Bank", () => {
   // });
 
   it("stores the type of transaction that was made eg. credit or debit", () => {
-    bank.deposit(10.50);
-    bank.withdraw(5.50);
-    bank.withdraw(2.00);
+    bank.deposit(10.5);
+    bank.withdraw(5.5);
+    bank.withdraw(2.0);
     expect(bank.getTransactions()).toEqual([
-      {type: "credit", amount: "10.50", balance: 10.50},
-      {type: "debit", amount: "5.50", balance: 5.00},
-      {type: "debit", amount: "2.00", balance: 3.00}
+      { type: "credit", amount: "10.50", balance: 10.5 },
+      { type: "debit", amount: "5.50", balance: 5.0 },
+      { type: "debit", amount: "2.00", balance: 3.0 },
     ]);
   });
 
   it("stores the balance of when the money was deposited into bank", () => {
-   bank.deposit(10.00);
-   expect(bank.getTransactions()).toEqual([
-     {type: "credit", amount: "10.00", balance: 10.00}
+    bank.deposit(10.0);
+    expect(bank.getTransactions()).toEqual([
+      { type: "credit", amount: "10.00", balance: 10.0 },
     ]);
   });
 
   it("stores the date of when the money is withdrawn from bank", () => {
-    bank.deposit(10.00, "14/01/2023");
-    bank.withdraw(5.00, "15/01/2023");
+    bank.deposit(10.0, "14/01/2023");
+    bank.withdraw(5.0, "15/01/2023");
     expect(bank.getTransactions()).toEqual([
-      {type: "credit", date: "14/01/2023", amount: "10.00", balance: 10.00},
-      {type: "debit", date: "15/01/2023", amount: "5.00", balance: 5.00}
+      { type: "credit", date: "14/01/2023", amount: "10.00", balance: 10.0 },
+      { type: "debit", date: "15/01/2023", amount: "5.00", balance: 5.0 },
     ]);
   });
 
   it("prints the statement of all transactions", () => {
     bank.deposit(10.35, "14/01/2023");
-    bank.withdraw(5.10, "15/01/2023");
+    bank.withdraw(5.1, "15/01/2023");
     expect(bank.printStatement()).toBe(
       "date || credit || debit || balance\n14/01/2023 || 10.35 ||  || 10.35\n15/01/2023 ||  || 5.10 || 5.25"
     );
-  })
-})
+  });
+});
